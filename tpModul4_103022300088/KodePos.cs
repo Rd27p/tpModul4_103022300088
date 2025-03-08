54bc1e5462790bb85a2496ac2cf42afce50783fd
Rd27p <g.a.raka.darma.p.k@gmail,com>
2025-03-08 17:07:27 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpModul4_103022300088/KodePos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpModul4_103022300088
+{
+    class KodePos
+    {
+        public enum Daerah 
+        {
+            Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja
+        }
+        public static int getKodePos(Daerah daerah)
+        {
+            int[] KodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+            return KodePos[(int)daerah];
+        }
+    }
+}   
--- tpModul4_103022300088/Program.cs
+++ tpModul4_103022300088/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace tpModul4_103022300088
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Kelurahan    Kode Pos");
+            Console.WriteLine(" ");
+
+            foreach (KodePos.Daerah daerah in Enum.GetValues(typeof(KodePos.Daerah)))
+            {
+                string namaDaerah = daerah.ToString();
+                Console.WriteLine($"{namaDaerah,-12} {KodePos.getKodePos(daerah)}");
+            }
+        }
+    }
+}
